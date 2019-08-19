c9942605b5f2539ea321f6dc6fc5d73d9cc228dc
Olivier Trepanier <jeparlefrancais21@gmail.com>
2019-08-19 16:16:24 -0700

    Remove arguments to roblox installer

--- bin/install-studio.py
+++ bin/install-studio.py
@@ -2,10 +2,14 @@ import os
 import sys
 import subprocess
 
-file_name = 'api.json'
+local_app_data = os.getenv('LOCALAPPDATA')
+
+print('LOCALAPPDATA =', local_app_data)
+
+roblox = os.path.join(local_app_data, 'Roblox')
 
 print('Install Roblox...')
-install_process = subprocess.Popen(['rbxinstall.exe', '-API', file_name])
+install_process = subprocess.Popen(['rbxinstall.exe'])
 
 timeout_count = 0
 
@@ -14,15 +18,9 @@ while timeout_count < 30:
         install_process.wait(timeout=2)
     except subprocess.TimeoutExpired:
         timeout_count += 1
-        print('is file written?', os.path.exists(file_name))
+        print('Retry?', not os.path.exists(roblox))
 
-print('Too much tries')
-
-local_app_data = os.getenv('LOCALAPPDATA')
-
-print('LOCALAPPDATA =', local_app_data)
-
-roblox = os.path.join(local_app_data, 'Roblox')
+print('Studio install expired.')
 
 if os.path.exists(roblox):
     print('Roblox content:')
