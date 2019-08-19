302b2a5b62b941b23ca75fadc01bf32c5bd5fb49
Olivier Trepanier <jeparlefrancais21@gmail.com>
2019-08-19 13:50:44 -0700

    Add pip to python install

--- bin/install-python.bat
+++ bin/install-python.bat
@@ -1,3 +1,3 @@
 curl https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe --output python-installer.exe
-python-installer.exe /quiet PrependPath=1 Include_doc=0
+python-installer.exe /quiet PrependPath=1 Include_pip=1 Include_doc=0
 rm python-installer.exe
